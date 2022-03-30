67c3c6f608170e22ee13635a80b40a3afb159972
Michel Metran <michelmetran@gmail.com>
2022-03-30 12:38:36 -0300

    Ajustes

--- src/app.py
+++ src/app.py
@@ -39,9 +39,6 @@ app.layout = html.Div(children=[
                 {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
                 {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
             ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
         }
     ),
 
