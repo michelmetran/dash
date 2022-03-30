b936c3869c828e48d05a8df7f009748373721722
Michel Metran <michelmetran@gmail.com>
2022-03-30 12:38:05 -0300

    Ajustes

--- src/app.py
+++ src/app.py
@@ -1,7 +1,4 @@
 import os
-# import dash
-# import dash_core_components as dcc
-# import dash_html_components as html
 from dash import Dash, dcc, html, Input, Output
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -12,20 +9,29 @@ server = app.server
 
 app.layout = html.Div(children=[
     #
-    html.H1(children='Hello World!'),
+    html.H1(children='Título 1'),
 
-    html.H2('Hello World'),
+    #
+    html.H2('Título 2'),
 
+    # Uma div aleatória
+    html.Div(children='Dash: A web application framework for Python.'),
+
+    # Um espaço
+    html.Br(),
+
+    # Um dropdown
     dcc.Dropdown(
         ['LA', 'NYC', 'MTL'],
         value='LA',
         id='dropdown'
     ),
 
-    html.Div(children='Dash: A web application framework for Python.'),
-
+    #
     html.Div(id='display-value'),
 
+    # Um gráfico estático
+    html.H2('Gráfico Estático'),
     dcc.Graph(
         id='example-graph',
         figure={
@@ -37,13 +43,17 @@ app.layout = html.Div(children=[
                 'title': 'Dash Data Visualization'
             }
         }
-    )
+    ),
+
+    # Outros
+    #ddd
+
 ])
 
 
 @app.callback(
+    [Input('dropdown', 'value')],
     Output('display-value', 'children'),
-    [Input('dropdown', 'value')]
 )
 def display_value(value):
     return f'You have selected {value}'
