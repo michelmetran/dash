ad9943d57e4f8637b8bbd71507133c302bd93f7b
Michel Metran <michelmetran@gmail.com>
2022-08-02 19:59:28 -0300

    Ajustes

--- main.py
+++ main.py
@@ -2,14 +2,13 @@
 dddd
 """
 
-
-from my_dash.app_table_simples import app
+# from my_dash.app_table_simples import app
 from my_templates.sidebar_multiple.app_template import app
 
 server = app.server
 
 if __name__ == '__main__':
     app.run_server(
-        debug=True,
-        port=8053,
+        # debug=True,
+        # port=8053,
     )
